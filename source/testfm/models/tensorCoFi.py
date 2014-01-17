53fbfb3b5c9575a55d4bc8fc26ac622db124c8d0
joaonrb <joaonrb@gmail.com>
2014-01-17 13:44:30 +0100

    [DOC] Some documentation added

--- source/testfm/models/tensorCoFi.py
+++ source/testfm/models/tensorCoFi.py
@@ -26,13 +26,43 @@ FloatMatrix = autoclass('org.jblas.FloatMatrix')
 
 
 class TensorCoFiModel(ModelInterface):
+    '''
+    TensorCoFi Model generator from the system database.
+    '''
 
-    def __init__(self,host,user,psw,dim=20, nIter=5, lamb=0.05, alph=40,
-        port=3306,dbname='raqksixq_ffosv1'):
+    def __init__(self,host,dbname,user,psw,dim=20, nIter=5, lamb=0.05, alph=40,
+        port=3306,):
         '''
         Python model creator fro tensor implementation in java
 
         **Args**
+
+            *String* host:
+                Database url or ip.
+
+            *String* dbname:
+                The name of the database using to fetch data
+
+            *String* user:
+                Username for database authentication.
+
+            *String* psw:
+                User password for authentication purposes.
+
+            *int* dim:
+                Dimension of some kind. Default = 20.
+
+            *int* nIter:
+                Nmber of iteration. Default = 5.
+
+            *float* lamb:
+                Lambda value for the algorithm. Default = 0,05.
+
+            *int* alph:
+                Alpha number for the algorithm. Default = 40.
+
+            *int* port:
+                Port number for the database. Default = 3306.
 
         **Return**
 
@@ -44,9 +74,15 @@ class TensorCoFiModel(ModelInterface):
         self.users, self.apps = self.getModel()
 
     def getData(self):
+        '''
+        Get the data from the database
+        '''
         return self.reader.getData()
 
     def getModel(self):
+        '''
+        Return the model
+        '''
         self.model.train(self.getData())
         final_model = self.model.getModel()
         t0 = numpy.fromiter(final_model.get(0).toArray(),dtype=numpy.float)
@@ -56,13 +92,17 @@ class TensorCoFiModel(ModelInterface):
         return t0, t1
 
     def getScore(self,user,item):
+        '''
+        Get a app score for a given user
+        '''
         return (self.users.transpose()[user.pk-1] * self.apps)[item.pk-1]
 
 if __name__ == '__main__':
     HOST = '192.168.188.128'
     USER = 'raqksixq_frappe'
     PSW = 'sp21o61h4'
-    t = TensorCoFiModel(HOST,USER,PSW)
+    DB = 'raqksixq_ffosv1'
+    t = TensorCoFiModel(HOST,DB,USER,PSW)
     print t
 
 
---
+++ source/testfm/splitter/holdout.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 17 of January 2014
+
+Holdout splitter method
+
+.. moduleauthor:: joaonrb <joaonrb@gmail.com>
+'''
+__author__ = {
+    'name':'joaonrb',
+    'e-mail': 'joaonrb@gmail.com'
+}
+__version__ = 1,0,0
+__since__ = 17,1,2014
+
+class Holdout(object):
+    '''
+    Holdout class for split
+    '''
