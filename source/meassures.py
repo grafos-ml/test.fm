21561d62f9f4078070d8a01f6c32fc89a1fa13d3
Linas <linas.baltrunas@gmail.com>
2014-01-20 16:21:03 +0100

    [MINOR] simplification

--- source/meassures.py
+++ source/meassures.py
@@ -39,21 +39,21 @@ class MAP_measure(Measure):
     def measure(self, recs):
         '''
         Example of how to use map and the input format.
-        >>> map = MAP_measure()
-        >>> map.measure([])
+        >>> mapm = MAP_measure()
+        >>> mapm.measure([])
         nan
 
-        >>> map.measure([(True, 0.00)]) #this is a perfect ranking of a single relevant item
+        >>> mapm.measure([(True, 0.00)]) #this is a perfect ranking of a single relevant item
         1.0
 
-        >>> map.measure([(False, 0.00)])
+        >>> mapm.measure([(False, 0.00)])
         0.0
 
-        >>> map.measure([(False, 0.01), (True, 0.00)])
+        >>> mapm.measure([(False, 0.01), (True, 0.00)])
         0.5
 
         The following example is taken from wikipedia (as far as I remember)
-        >>> map.measure([(False, 0.9), (True, 0.8), (False, 0.7), (False, 0.6), (True, 0.5), \
+        >>> mapm.measure([(False, 0.9), (True, 0.8), (False, 0.7), (False, 0.6), (True, 0.5), \
         (True, 0.4), (True, 0.3), (False, 0.2), (False, 0.1), (False, 0)])
         0.4928571428571428
         '''
@@ -61,18 +61,18 @@ class MAP_measure(Measure):
         if not recs or not isinstance(recs, list) or len(recs) < 1:
             return float('nan')
 
-        map = 0.0
+        mapm = 0.0
         relevant = 0.0
 
         for i, r in enumerate(recs):
             ground_truth, prediction = r
             if isinstance(ground_truth, bool) and ground_truth is True:
                 relevant += 1
-                map += relevant / (i+1)
+                mapm += relevant / (i+1)
         if relevant == 0:
             return 0.0
         else:
-            return map/relevant
+            return mapm/relevant
 
 
 class Precision_measure(Measure):
@@ -98,7 +98,6 @@ class Precision_measure(Measure):
         0.4
         '''
 
-        map = 0.0
         relevant = 0.0
 
         if not recs or not isinstance(recs, list) or len(recs) < 1:
