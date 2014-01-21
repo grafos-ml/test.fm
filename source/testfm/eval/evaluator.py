d9ec0934df3c1a5bcb3b29cf9ea6d571f1007624
Linas <linas.baltrunas@gmail.com>
2014-01-21 20:06:19 +0100

    added possibility to provide all items in the training set

--- source/testfm/eval/evaluator.py
+++ source/testfm/eval/evaluator.py
@@ -7,47 +7,60 @@ from testfm.models.interface import ModelInterface
 from testfm.models.fake_model import IdModel
 from pandas import DataFrame
 '''
-Takes the model, testing data and evaluation meassure and spits out the score.
+Takes the model,testing data and evaluation measure and spits out the score.
 '''
 
 class Evaluator(object):
 
-    def evaluate_model(self, factor_model, testing_dataframe, measures=[MAP_measure()], non_relevant=100):
+    def evaluate_model(self, factor_model, testing_dataframe, measures=[MAP_measure()],
+                       all_items=None, non_relevant_count=100):
         """
         Evaluates the model by the following algorithm:
             1. for each user:
                 2. take all relevant items from the testing_dataframe
                 3. inject #non_relevant random items
-                4. predict the score for each
-                5. sort according to the score
+                4. predict the score for each item
+                5. sort according to the predicted score
                 6. evaluate according to each measure
             7.average the scores for each user
         >>> mapm = MAP_measure()
         >>> model = IdModel()
         >>> eval = Evaluator()
         >>> df = DataFrame({'user' : [1, 1, 3, 4], 'item' : [1, 2, 3, 4], 'rating' : [5,3,2,1], 'date': [11,12,13,14]})
-        >>> len(eval.evaluate_model(model, df, non_relevant=2))
+        >>> len(eval.evaluate_model(model, df, non_relevant_count=2))
         1
 
         #not the best test, I need to put seed in order to get an expected behaviour
 
         :param factor_model: ModelInterface  an instance of ModelInterface
         :param testing_dataframe: DataFrame pandas dataframe of testing data
-        :param measures: list a list of measure we want to compute (instnces of)
+        :param measures: list of measure we want to compute (instnces of)
+        :param all_items: list of items available in the data set (used for negative sampling).
+         If set to None, then testing items are used for this
+        :param non_relevant_count: int number of non relevant items to add to the list for performance evaluation
         :return: list of score corresponding to measures
         """
 
         if not isinstance(factor_model, ModelInterface):
             raise ValueError("Factor model should be an instance of ModelInterface")
+
         if not isinstance(testing_dataframe, DataFrame):
             raise ValueError("Testing data should be a pandas dataframe")
+
+        if not 'item' in testing_dataframe.columns:
+            raise ValueError("Testing data should be a pandas dataframe with 'item' column")
+        if not 'user' in testing_dataframe.columns:
+            raise ValueError("Testing data should be a pandas dataframe with 'user' column")
+
         for m in measures:
             if not isinstance(m, Measure):
                 raise ValueError("Measures should contain only Measure instances")
 
         partial_measures = {}#a temp dictionary to store partial measures
 
-        items = testing_dataframe.item.unique()
+        if all_items is None:
+            all_items = testing_dataframe.item.unique()
+
 
         #1. for each user:
         grouped = testing_dataframe.groupby('user')
@@ -59,7 +72,7 @@ class Evaluator(object):
                 ranked_list.append((True, prediction))
 
             #3. inject #non_relevant random items
-            for nr in sample(items, non_relevant):
+            for nr in sample(all_items, non_relevant_count):
                 prediction = factor_model.getScore(user,nr)
                 ranked_list.append((False, prediction))
 
