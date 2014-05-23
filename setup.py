10d8d3551f25f9573f51b113904eda4acc13645c
joaonrb <joaonrb@gmail.com>
2014-05-23 13:05:28 +0200

    [IMPROVMENT] Change on setup. Now try to find the libraries in the standard places and ships with the headers.

--- setup.py
+++ setup.py
@@ -6,37 +6,71 @@ import pip
 from distutils.core import setup
 from setuptools import find_packages
 from distutils.extension import Extension
-from pkg_resources import WorkingSet, DistributionNotFound
-working_set = WorkingSet()
 try:
-    dep = working_set.require("cython")
-except DistributionNotFound:
+    from Cython.Distutils import build_ext
+except ImportError:
     pip.main(["install", "cython"])
+    from Cython.Distutils import build_ext
 try:
-    dep = working_set.require("numpy")
-except DistributionNotFound:
+    import numpy as np
+except ImportError:
     pip.main(["install", "numpy"])
-from Cython.Distutils import build_ext
-import numpy as np
+    import numpy as np
+
+STD_ATLAS_LIB = "/usr/lib/atlas-base/atlas"
+STD_LIB = "/usr/lib"
 
 
-ATLAS_LIB = None
-ATLAS_INCLUDE = None
-if sys.platform == "linux" or sys.platform == "linux2":
-    ATLAS_LIB = "/usr/lib/atlas-base/atlas"
-    ATLAS_INCLUDE = "/usr/include/atlas"
-elif sys.platform == "darwin":
-    ATLAS_LIB = "/usr/lib/atlas"
-    ATLAS_INCLUDE = "/usr/include/atlas"
-#elif sys.platform == "win32":
-#    ATLAS_LIB = "/usr/lib/atlas-base/atlas"
-#    ATLAS_INCLUDE = "/usr/include/atlas"
+def search_for(file_name, location):
+    """
+    Search for the path of file
+    """
+    result = None
+    for root, dirs, files in os.walk(location):
+        print "searching at %s" % root
+        if file_name in files:
+            print " found it"
+            result = root
+            break
+    return result
 
-# Any value in env for ATLAS_LIB or ATLAS_INCLUDE will substitute the previous default python
-ATLAS_LIB = os.environ.get("ATLAS_LIB", None) or ATLAS_LIB
-ATLAS_INCLUDE = os.environ.get("ATLAS_INCLUDE", None) or ATLAS_INCLUDE
-if not ATLAS_INCLUDE or not ATLAS_LIB:
-    raise OSError("OS not supported yet")
+
+def find_blas():
+    """
+    Try to find the blas library file in the standard libraries
+    """
+    if sys.platform == "linux" or sys.platform == "linux2":
+        blas = "libblas.so"
+    elif sys.platform == "darwin":
+        blas = "libblas.a"
+    else:
+        raise OSError("OS not supported yet")
+    result = search_for(blas, STD_ATLAS_LIB) or search_for(blas, STD_LIB)
+    print result
+    if result is None:
+        raise EnvironmentError("Cannot find %s" % blas)
+    return result
+
+
+def find_lapack():
+    """
+    Try to find the lapack library file in the standard libraries
+    """
+    if sys.platform == "linux" or sys.platform == "linux2":
+        lapack = "liblapack.so"
+    elif sys.platform == "darwin":
+        lapack = "liblapack.a"
+    else:
+        raise OSError("OS not supported yet")
+    result = search_for(lapack, STD_ATLAS_LIB) or search_for(lapack, STD_LIB)
+
+    if result is None:
+        raise EnvironmentError("Cannot find %s" % lapack)
+    return result
+
+
+BLASLIB = os.environ.get("BLASLIB", find_blas())
+LAPACKLIB = os.environ.get("LAPACKLIB", find_lapack())
 
 
 ext_modules = [
@@ -50,13 +84,13 @@ ext_modules = [
               libraries=["python2.7"]),
     Extension("testfm.models.cutil.float_matrix", ["src/testfm/models/cutil/float_matrix.pyx"],
               libraries=["lapack", "cblas", "python2.7"],
-              library_dirs=[ATLAS_LIB],
-              include_dirs=[ATLAS_INCLUDE]),
+              library_dirs=[BLASLIB, LAPACKLIB],
+              include_dirs=["./include"]),
     Extension("testfm.models.cutil.int_array", ["src/testfm/models/cutil/int_array.pyx"]),
     Extension("testfm.models.cutil.tensorcofi", ["src/testfm/models/cutil/tensorcofi.pyx"],
               libraries=["cblas", "python2.7"],
-              library_dirs=[ATLAS_LIB],
-              include_dirs=[ATLAS_INCLUDE, np.get_include()]),
+              library_dirs=[BLASLIB, LAPACKLIB],
+              include_dirs=["./include", np.get_include()]),
     Extension("testfm.models.cutil.baseline_model", ["src/testfm/models/cutil/baseline_model.pyx"],
               libraries=["python2.7"]),
 ]
